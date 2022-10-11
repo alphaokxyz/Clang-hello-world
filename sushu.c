195bbc75fab2edccfc4f81511e13f978bd54f2f1
alphaokxyz <93239194+alphaokxyz@users.noreply.github.com>
2022-10-11 16:06:21 +0800

    Add files via upload

---
+++ sushu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    int i;
+    int isPrime = 1;
+    for ( i=2; i<x; i=i+1 ) {
+        if ( x % i == 0 ) {
+            isPrime = 0;
+        }
+    }
+    if ( isPrime == 1 ) {
+        printf("是素数\n");
+    }
+    else {
+        printf("不是素数\n");
+    }
+    return 0;
+}
